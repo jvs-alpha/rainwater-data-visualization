ca21b1506e6845e1a363c7d137e2aff67d4a41a1
jvs-alpha <jvsamuel.rufus@gmail.com>
2019-07-19 07:32:55 +0530

    added changes

--- data_read_test2.py
+++ data_read_test2.py
@@ -6,12 +6,13 @@ csv_dat = pd.read_csv("chennai_reservoir_rainfall_made1.csv")
 read_dat = dict(csv_dat)
 formed_dat = {"Date":[],"POONDI":[],"CHOLAVARAM":[],"REDHILLS":[],"CHEMBARAMBAKKAM":[]}
 check = 0
+on_read = None
 for date in read_dat["Date"]:
     if date not in formed_dat["Date"]:
-        run_on = date
         formed_dat["Date"].append(date)
+        for dat2 in read_dat["Date"]:
+            if dat2 == 
 
-    if 
     check += 1
 
 #print(formed_dat)
