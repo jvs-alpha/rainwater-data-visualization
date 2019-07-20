c8fae8a8851954f5b902bcb8f0ead7b578b86bb5
jvs-alpha <jvsamuel.rufus@gmail.com>
2019-07-20 16:27:27 +0530

    found a way to add data

--- data_read_test2.py
+++ data_read_test2.py
@@ -1,19 +1,19 @@
-import csv
 import pandas as pd
 import matplotlib.pyplot as plt
 
 csv_dat = pd.read_csv("chennai_reservoir_rainfall_made1.csv")
 read_dat = dict(csv_dat)
-formed_dat = {"Date":[],"POONDI":[],"CHOLAVARAM":[],"REDHILLS":[],"CHEMBARAMBAKKAM":[]}
-check = 0
-on_read = None
+mod_dat = {"Date":[],"POONDI":[],}
 for date in read_dat["Date"]:
-    if date not in formed_dat["Date"]:
-        formed_dat["Date"].append(date)
-        for dat2 in read_dat["Date"]:
-            if dat2 == 
+    if date not in mod_dat["Date"]:
+        mod_dat["Date"].append(date)
 
-    check += 1
-
-#print(formed_dat)
-print(check)
+sum = 0
+for date in mod_dat["Date"]:
+    for check in range(0,5647):
+        if read_dat["Date"][check] == date:
+            sum += read_dat["POONDI"][check]
+    mod_dat["POONDI"].append(sum)
+    sum = 0
+mod_dat = pd.DataFrame(mod_dat)
+mod_dat.to_csv("chennai_reservoir_rainfall_made2.csv")
---
+++ test.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+read_dat = pd.read_csv("chennai_reservoir_rainfall_made1.csv")
+sum = 0
+for val in read_dat["POONDI"]:
+    sum += val
+
+avg = sum/5647
+print(avg)
